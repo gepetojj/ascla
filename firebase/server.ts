822cbbe94d8edbb116db9d698205613f36a27f0f
Gepetojj <nmjoaopedro22@gmail.com>
2022-06-29 12:36:16 -0300

    fix(firebase/server.ts): alteração no método de ler a chave privada do firebase

--- firebase/server.ts
+++ firebase/server.ts
@@ -2,7 +2,7 @@ import firebase, { ServiceAccount, credential } from "firebase-admin";
 
 const credentials: ServiceAccount = {
 	projectId: process.env.FIREBASE_PROJECT_ID,
-	privateKey: process.env.FIREBASE_PRIVATE_KEY,
+	privateKey: String(process.env.FIREBASE_PRIVATE_KEY).replace(/\\n/g, "\n"),
 	clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
 };
 
