0ea337b30dd5016483f289e36eadbfeffdc43ccd
Gepetojj <nmjoaopedro22@gmail.com>
2022-06-30 18:01:03 -0300

    refactor(pages/_app.tsx): adição do script do Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import { SessionProvider } from "next-auth/react";
 import { DefaultSeo } from "next-seo";
 import type { AppProps } from "next/app";
 import { useRouter } from "next/router";
+import Script from "next/script";
 import React, { useMemo } from "react";
 import { ReactNotifications } from "react-notifications-component";
 import "styles/fonts.css";
@@ -35,6 +36,26 @@ export default function App({ Component, pageProps: { session, ...pageProps } }:
 				<ReactNotifications />
 				{Layout}
 			</SessionProvider>
+
+			<Script
+				strategy="afterInteractive"
+				src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+			/>
+			<Script
+				id="gtag-init"
+				strategy="afterInteractive"
+				/* # skipcq: JS-0440 */
+				dangerouslySetInnerHTML={{
+					__html: `
+            			window.dataLayer = window.dataLayer || [];
+           				function gtag(){dataLayer.push(arguments);}
+            			gtag('js', new Date());
+            			gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+              				page_path: '${pathname}',
+           				});
+          			`,
+				}}
+			/>
 		</>
 	);
 }
