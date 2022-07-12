3d6560e094cbc9f32962715ee00f55ddc78c52a0
Gepetojj <nmjoaopedro22@gmail.com>
2022-07-12 12:15:06 -0300

    refactor: remoção do plugin include_sharp

--- include_sharp/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	async onBuild({ netlifyConfig }) {
-		const fn = netlifyConfig.functions["___netlify-handler"];
-
-		fn.included_files = fn.included_files.filter(f => f !== "!node_modules/sharp/**/*");
-	},
-};
