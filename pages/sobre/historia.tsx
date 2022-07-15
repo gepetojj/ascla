0e745e202b50bc603168a9638afa9c44956a9ec4
Gepetojj <nmjoaopedro22@gmail.com>
2022-07-15 12:22:17 -0300

    feat(sobre/historia.tsx): implementação da história da academia

--- pages/sobre/historia.tsx
+++ pages/sobre/historia.tsx
@@ -1,5 +1,6 @@
 import { Main } from "components/layout/Main";
 import { config } from "config";
+import { IKImage } from "imagekitio-react";
 import type { NextPage } from "next";
 import { NextSeo } from "next-seo";
 import React from "react";
@@ -18,40 +19,82 @@ const AboutHistory: NextPage = () => {
 			>
 				<div className="prose max-w-5xl text-justify">
 					<p>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Morbi tristique senectus
-						et netus et malesuada fames ac. Ut sem nulla pharetra diam sit. Congue
-						quisque egestas diam in. Purus viverra accumsan in nisl nisi scelerisque eu.
-						A arcu cursus vitae congue mauris rhoncus aenean vel. Vel turpis nunc eget
-						lorem dolor sed viverra. Vestibulum lorem sed risus ultricies tristique
-						nulla. Convallis tellus id interdum velit laoreet id donec ultrices
-						tincidunt. Tempor orci eu lobortis elementum nibh tellus molestie nunc non.
-						Velit laoreet id donec ultrices tincidunt arcu non. Ac tortor vitae purus
-						faucibus ornare suspendisse sed. Lacinia quis vel eros donec ac odio tempor
-						orci. Aliquam malesuada bibendum arcu vitae elementum. Blandit libero
-						volutpat sed cras ornare arcu dui. Cursus risus at ultrices mi tempus
-						imperdiet nulla malesuada pellentesque. Proin sagittis nisl rhoncus mattis
-						rhoncus. Mus mauris vitae ultricies leo integer malesuada nunc vel risus.
-						Proin sagittis nisl rhoncus mattis rhoncus urna. Arcu non sodales neque
-						sodales ut etiam sit. Tempus urna et pharetra pharetra massa massa ultricies
-						mi. Ipsum suspendisse ultrices gravida dictum fusce. In iaculis nunc sed
-						augue lacus viverra. Vitae turpis massa sed elementum tempus egestas sed.
-						Duis ut diam quam nulla porttitor massa id neque aliquam. Tempor orci eu
-						lobortis elementum. Aenean pharetra magna ac placerat vestibulum lectus
-						mauris ultrices eros. Tortor posuere ac ut consequat semper.
+						O Portal Maltanet, do jornalista José Malta Fontes Neto, teve um papel
+						fundamental na expansão das produções literárias no sertão na década de
+						1990. Muito mais que isso! O veículo informativo e noticioso, inovou ao
+						criar um mural de recado, encurtou a distância entre os santanenses
+						espalhados pelo Brasil afora.
 					</p>
-					<h2 className="text-center">Molestie Nunc</h2>
 					<p>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Fermentum posuere urna
-						nec tincidunt praesent semper. Leo vel fringilla est ullamcorper eget nulla
-						facilisi. Risus pretium quam vulputate dignissim suspendisse. Blandit libero
-						volutpat sed cras ornare. Vulputate ut pharetra sit amet aliquam id diam
-						maecenas ultricies. Vitae proin sagittis nisl rhoncus. Est ullamcorper eget
-						nulla facilisi etiam dignissim diam quis enim. Sit amet est placerat in
-						egestas erat imperdiet. Ultrices in iaculis nunc sed augue lacus viverra
-						vitae. Rutrum quisque non tellus orci ac auctor augue mauris.
+						Ao mesmo tempo, noticiou ao mundo digital que brotava uma nascente de ideias
+						que não mais parou de jorrar até os dias atuais, inspirada e guiada nos
+						exemplos de Tadeu Rocha, Breno Accioly, José Marques Melo, Oscar Silva, Raul
+						Pereira Monteiro, Clerisvaldo Braga das Chagas e Djalma Carvalho,
+						despontando uma geração de novos escritores que resultou na publicação da
+						série de livros coletivos: À Sombra do Umbuzeiro (2006), À Sombra do
+						Juazeiro (2008) e À Sombra da Quixabeira (2010).
 					</p>
+					<p>
+						Os participantes do movimento literário se uniram para fortalecer o
+						movimento de incentivo à leitura entre os jovens estudantes da rede
+						municipal de ensino e reeditar obras relevantes, como por exemplo, “Escorço
+						Biográfico do Missionário Apostólico Dr. Francisco José Correia de
+						Albuquerque”, do Padre Theotônio Ribeiro, 2012, lançado pela primeira
+						editora do sertão alagoano SWA Instituto Educacional Ltda, que também surgiu
+						com a motivação e o impulso das demandas dos sertanejos sedentos em
+						concretizar os sonhos literários em livros.
+					</p>
+					<p>
+						Por ocasião da realização da I FELISI - Feira Literária de Santana do
+						Ipanema, realizada na secretaria de educação municipal, no período de 29 de
+						Maio a 01 de junho de 2012, aconteceu o marco histórico de homenagem aos
+						escritores santanenses e a fundação da ASLCA - Academia Santanense de
+						Letras, Ciências e Artes.
+					</p>
+					<p>
+						Na FELISI aconteceu exposição de fotografias, telas e árvores literárias,
+						contação de histórias, homenagens das escolas municipais a escritores,
+						pinturas e tatuagens infantis, fantoches, oficina de pinturas, participação
+						dos cordelistas, cantoria de repentistas, sussurradores de poesia, shows
+						artísticos; oficinas informática, oficina de cordel, mostra de cinema, roda
+						de conversa, cordel para gente miúda, reciclagem, oficina de poesia, oficina
+						de produção literária, origami, matemática em cordel, mesas redondas,
+						palestras e shows artísticos.
+					</p>
+					<p>
+						Presente no evento Dr. Jucá Santos, jornalista, advogado, presidente da
+						Academia Maceioense de Letras - AML por várias décadas, ocupante da cadeira
+						nº 32 da Academia Alagoana de Letras - AAL. Conhecido no meio literário como
+						o Príncipe dos Poetas de Maceió, maçom integrante da Loja Maçônica Virtude e
+						Bondade do Grande Oriente do Brasil de Maceió entusiasta das letras e
+						apoiador das Academias Literárias na Terra dos Marechais, presente na
+						reunião que homenageou os escritores santanenses na I FELISI conclamou a
+						então prefeita Renilde Bulhões para fundar a Academia de Letras e conseguiu,
+						naquele momento estava fundada a Academia Santanense de Letras, Ciências e
+						Artes.
+					</p>
+					<div className="flex justify-center items-center">
+						<figure>
+							<IKImage
+								urlEndpoint={process.env.NEXT_PUBLIC_IK_URL}
+								path="/fundacao-ascla.webp"
+								transformation={[{ width: 450, height: 250 }]}
+								lqip={{ active: true }}
+								loading="lazy"
+								width={450}
+								height={250}
+								alt="Imagem do evento da fundação (29/08/2012)"
+							/>
+							<figcaption className="text-center">
+								Evento da fundação (29/08/2012)
+							</figcaption>
+						</figure>
+					</div>
+					<div className="flex">
+						<strong className="w-full text-right">
+							José Malta Fontes Neto, presidente da ASLCA, 29.05.2012
+						</strong>
+					</div>
 				</div>
 			</Main>
 		</>
